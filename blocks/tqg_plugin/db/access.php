bf70d6d15d487898d5ae67e12bc98eadd761db9d
Arthur <arthur_adolfo@hotmail.com>
2020-10-13 18:13:59 -0300

    feat: add block sample

---
+++ blocks/tqg_plugin/db/access.php
@@ -0,0 +1,26 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+    'block/tqg_plugin:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+
+    'block/tqg_plugin:manage' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+    ]
+);
